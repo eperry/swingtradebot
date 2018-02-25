4bcedf7e416eefef390a9649ee2bb68559fdb942
Ed Perry <edwardperry1@gmail.com>
2018-02-25 09:21:07 -0500

    update

--- test/test-websock-ticker-channel.js
+++ test/test-websock-ticker-channel.js
@@ -1,25 +1,16 @@
 var gdax = require('gdax');
-var gdaxConfig = require('./gdax.config')
+var gdaxConfig = require('./../config/gdax.config')
 var direction=0;
 //websocket = new gdax.WebsocketClient(['BTC-USD'],gdaxConfig.apiURI,null,{ 'channels':['level2']});
-websocket = new gdax.WebsocketClient(['BTC-USD'],"wss://ws-feed-public.sandbox.gdax.com",null,{ 'channels': ['ticker']});
-var snapshots = {
-	asks: {},
-	bids: {}	
-};
-function sell(data){
-	data.shift();
-	snapshots.asks[data[0]]=0;
-	snapshots.asks[data[0]]+=parseFloat(data[1]);
-	//console.log(data[0]+"="+snapshots.asks[data[0]]+"--"+data[1]);
-}
-function buy(data){
-	data.shift();
-	snapshots.bids[data[0]]=0;
-	snapshots.bids[data[0]]+=parseFloat(data[1]);
-}
+//websocket = new gdax.WebsocketClient(['BTC-USD'],"wss://ws-feed-public.sandbox.gdax.com",null,{ 'channels': ['ticker']});
+websocket = new gdax.WebsocketClient(
+				['BTC-USD'],
+				"wss://ws-feed.gdax.com",
+				null,
+				{ 'channels': ['ticker']}
+				);
 websocket.on('message', data => { 
-		if( data.type !== 'heartbeat' ) console.log(JSON.stringify(data,null,1))
+	if( data.type !== 'heartbeat' ) console.log(JSON.stringify(data,null,1))
 });
 websocket.on('error', err => { /* handle error */ });
 websocket.on('close', () => { /* ... */ });
