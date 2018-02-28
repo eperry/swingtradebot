bac615dc63386e480acd8ebd109bb1aa2e43c0c5
Ed Perry <edwardperry1@gmail.com>
2018-02-27 22:10:02 -0500

    update

--- test/test-getAccountTransfers.js
+++ test/test-getAccountTransfers.js
@@ -1,8 +1,7 @@
 var Gdax = require('gdax');
 var gdaxconfig = require('../config/gdax.config')
 //console.log(gdaxconfig);
-const authedClient = new Gdax.AuthenticatedClient(gdaxconfig.key, gdaxconfig.secret, gdaxconfig.passphrase, gdaxconfig.apiURI);
-//console.log(authedClient)
+var authedClient = new Gdax.AuthenticatedClient(gdaxconfig.key, gdaxconfig.secret, gdaxconfig.passphrase, gdaxconfig.apiURI);
 
 new Promise(function(resolve, reject) {
 	var value = 0;
@@ -16,21 +15,45 @@ new Promise(function(resolve, reject) {
 		    hold: '0.0000000000000000',
 		    profile_id: '48744b90-c75f-43c4-b5c4-0c10c6dc644e' } ]
 		**********************************************************************/
-		if(err) console.log(err.data);
+		if(err) console.log(err);
 		if(data){
-			authedClient.getAccountTransfers(data.id, (error,response,data)=>{
-				if (error)	console.log(error)
-				console.log(data)
-			})
-			//console.log(data);
 			data.forEach((d) =>{
-				if ( d.currency === 'USD' ) value+=parseFloat(d.available)
-				if ( d.currency === 'BTC' ) size +=parseFloat(d.available)
+				console.log(d.id);
+				authedClient.getAccountTransfers(d.id, (error,response,da)=>{
+					if (error)	console.log(error)
+					if (da){ da.forEach((t) => {
+						/*
+						{"id":"b80f6f7c-1dfb-420e-a978-93a7add0eaa1",
+							"type":"deposit",
+							"created_at":"2018-02-03 03:33:47.111733+00",
+							"completed_at":"2018-02-03 03:33:47.124555+00",
+							"canceled_at":null,
+							"processed_at":"2018-02-03 03:33:47.124555+00",
+							"user_nonce":null,
+							"amount":"210000.00000000",
+							"details":{"coinbase_account_id":"bcdd4c40-df40-5d76-810c-74aab722b223",
+								"coinbase_transaction_id":"507f1f77bcf86cd799439011"
+							}
+						}
+						*/
+						if ( d.currency === 'USD' ){
+							 value+=parseFloat(t.amount)
+							 console.log(value,t)
+						}
+						if ( d.currency === 'BTC' ) size +=parseFloat(t.amount)
+						//console.log(JSON.stringify(t))
+					})
+					//console.log("==="+JSON.stringify(da))
+					}
+				})
+				//console.log(data);
+				//if ( d.currency === 'USD' ) value+=parseFloat(d.available)
+				//if ( d.currency === 'BTC' ) size +=parseFloat(d.available)
 			})
+			resolve({ 'value': value, 'size': size })
 			//console.log("ballance value = "+value+" ( "+size+" )")		
 		}
 	});
-	resolve({ 'value': value, 'size': size })
 }).then(function(result) { // (**)
 	console.log("Total ",JSON.stringify(result))
 })
