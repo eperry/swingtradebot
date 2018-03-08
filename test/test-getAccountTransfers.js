e9e87b29f65601080a501be0c6c6dfd10eaa7e3c
Ed Perry <edwardperry1@gmail.com>
2018-03-08 08:07:57 -0500

    update

--- test/test-getAccountTransfers.js
+++ test/test-getAccountTransfers.js
@@ -2,29 +2,39 @@ var Gdax = require('gdax');
 var gdaxconfig = require('../config/gdax.config')
 //console.log(gdaxconfig);
 var authedClient = new Gdax.AuthenticatedClient(gdaxconfig.key, gdaxconfig.secret, gdaxconfig.passphrase, gdaxconfig.apiURI);
-var accounts = []
+var total = 0 
 authedClient.getAccounts().then((ga)=>{
-            var p = [];
-	    for( i =0; i < ga.length;i++){
-		p.push(authedClient.getAccountTransfers(ga[i].id).then((result)=>{
-			    //console.log(result)
-			    var total = 0;
-			    result.forEach((r)=>{
-				if ( r.amount ) total+=parseFloat(r.amount)
-			    })
-			    return total;
-		    }))
-	    } // End for
-	    Promise.all(p).then((result)=>{
+         var p = [];
+	//console.log(ga)
+	 for( i =0; i < ga.length;i++){
+	    console.log("Trans balance "+ga[i].balance*-1)
+	    if ( ga[i].balance ) total-=parseFloat(ga[i].balance) ; 
+	    //console.log("-",total)
+	    p.push(authedClient.getAccountTransfers(ga[i].id).then((result)=>{
+		    //console.log(result)
+	//	    var total = 0;
+		    result.forEach((r)=>{
+			total+=parseFloat(r.amount) ; 
+			console.log("Amount "+r.amount) ; 
+		    })
+		    //console.log(total)
+		    return total;
+	    })
+	 )} // End for
+	 Promise.all(p).then((result)=>{
 	    //console.log(result)
-	    var total = result.reduce((accumulator, currentValue) =>  accumulator + currentValue)
+	    //var total =0;
+	    for ( i=0; i < result.length; i++){
+			total += result[i]
+	    }
 	    return total
 	 }).then ((result)=>{
-	    	console.log("total",result)
+		console.log("total",result)
 	 }).catch ((error)=>{
-	  	console.log(error)
+		console.log(error)
 	 })
+	    
 		
  }).catch ((error)=>{
-			console.log(error)
+	console.log(error)
  })
