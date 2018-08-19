2eb3985831471587da4517d31367495f7a0d4268
Ed Perry <edwardperry1@gmail.com>
2018-08-19 10:09:26 -0400

    update

--- test/test-fills.js
+++ test/test-fills.js
@@ -1,75 +1,39 @@
 var Gdax = require('gdax');
-var gdaxconfig = require('../config/gdax.config')
-//console.log(gdaxconfig);
+var sprintf = require('sprintf')
+var color = require('colors')
+var gdaxconfig = require('../config/profit.js.config')
+var size = 0
+var price = 0
+var value = 0
 const authedClient = new Gdax.AuthenticatedClient(gdaxconfig.key, gdaxconfig.secret, gdaxconfig.passphrase, gdaxconfig.apiURI);
-//console.log(authedClient)
-
-new Promise(function(resolve, reject) {
-	var value = 0;
-	var size  = 0;
-	authedClient.getAccounts( (err, response, data)=>{
-		/**********************************************************************
-		[{ id: '1d68bd74-655b-4627-a836-d336f6c80d5e',
-		    currency: 'BTC',
-		    balance: '0.0030974800000000',
-		    available: '0.00309748',
-		    hold: '0.0000000000000000',
-		    profile_id: '48744b90-c75f-43c4-b5c4-0c10c6dc644e' } ]
-		**********************************************************************/
-		if(err) console.log(err.data);
-		if(data){
-			//console.log(data);
-			data.forEach((d) =>{
-				if ( d.currency === 'USD' ) value+=parseFloat(d.available)
-				if ( d.currency === 'BTC' ) size +=parseFloat(d.available)
+function getFills( before ){
+	p =  { product_id: "LTC-USD"}
+	if ( before >0  ) p.before = before
+	//console.log(p)
+	authedClient.getFills(p ,(error,response,data)=>{
+		if (error) console.log(error)
+		if (data) {
+			//console.log(data)
+			data.forEach((d) => {
+				o = sprintf("%s %s %s",d.side,d.price,d.size)
+				if ( d.side === 'buy' ){
+					o = o.red
+					size += parseFloat(d.size)
+					value += parseFloat(d.size) * parseFloat(d.price)
+				} else {
+					o = o.blue
+					size -= parseFloat(d.size)
+					value -= parseFloat(d.size) * parseFloat(d.price)
+				}
+				price = ( price + parseFloat(d.price) ) / 2
+				console.log( o , sprintf(" price %f size %f value %f",price, size,value))
 			})
-			//console.log("ballance value = "+value+" ( "+size+" )")		
+			after = response.headers['cb-after'];
+			//console.log(after)
+			//console.log(response.headers)
+			if ( typeof after !== 'undefined' ) getFills(after)
+			else console.log( sprintf("price %f size %f",price, size))
 		}
 	});
-	authedClient.getFills((error,response,data)=>{
-		if (error)	console.log(error)
-		//console.log(data)
-		/*
-		 { created_at: '2018-02-05T13:38:51.275Z',
-		    trade_id: 652849,
-		    product_id: 'BTC-USD',
-		    order_id: 'a7f4253d-f0c4-4e98-9e40-663f08aaa08a',
-		    user_id: '599088a1aef8bb01138248f9',
-		    profile_id: '48744b90-c75f-43c4-b5c4-0c10c6dc644e',
-		    liquidity: 'M',
-		    price: '8902.99000000',
-		    size: '0.86600000',
-		    fee: '0.0000000000000000',
-		    side: 'buy',
-		    settled: true,
-		    usd_volume: '7709.9893400000000000' },
-		  { created_at: '2018-02-05T13:37:22.368Z',
-		    trade_id: 652848,
-		    product_id: 'BTC-USD',
-		    order_id: 'fb413c17-853d-4438-bf2c-ebdf05ae345c',
-		    user_id: '599088a1aef8bb01138248f9',
-		    profile_id: '48744b90-c75f-43c4-b5c4-0c10c6dc644e',
-		    liquidity: 'M',
-		    price: '10001.19000000',
-		    size: '0.97292854',
-		    fee: '0.0000000000000000',
-		    side: 'sell',
-		    settled: true,
-		    usd_volume: '9730.4431849626000000' },
-		*/
-		data.forEach((d) => {
-			if ( d.side === 'buy' ){
-				size  += ( parseFloat(d.size) )
-				value += ( d.price*d.size )
-			} else {
-				size  -= ( parseFloat(d.size) )
-				value -= ( d.price*d.size )
-			}
-			console.log(d.created_at+ " "+d.side+" Price "+d.price+" * "+d.size+" = "+(d.price*d.size) +" v="+value+" ( "+size+" )")		
-		})
-		resolve({ 'value': value, 'size': size })
-	});
-}).then(function(result) { // (**)
-	console.log("Total ",JSON.stringify(result))
-})
-
+}
+getFills(1);
